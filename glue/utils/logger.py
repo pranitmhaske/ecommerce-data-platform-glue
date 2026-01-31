75a5016e3c17e1a03dd7a1adf5fb5089c1a53506
Pranit Vijay Mhaske <pranitm099@gmail.com>
2026-01-31 08:00:37 +0530

    Update logger.py

--- glue/utils/logger.py
+++ glue/utils/logger.py
@@ -1,19 +1,17 @@
 import logging
 import sys
-from datetime import datetime
 
 def get_logger(name: str):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
 
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(logging.Formatter(
-        fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S"
-    ))
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(logging.Formatter(
+            fmt="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S"
+        ))
+        logger.addHandler(handler)
 
-    if logger.hasHandlers():
-        logger.handlers.clear()
-
-    logger.addHandler(handler)
+    logger.propagate = False
     return logger
